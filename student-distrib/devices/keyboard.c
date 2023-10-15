ccb5ad5cbc473b0e3c2914db56561f0a0174514d
Chiming Ni <chiming2@illinois.edu>
2023-10-15 06:42:44 -0500

    [Checkpoint 1] Simple keyboard handling support

--- student-distrib/devices/keyboard.c
+++ student-distrib/devices/keyboard.c
@@ -2,15 +2,55 @@
 #include "../lib.h"
 #include "../i8259.h"
 
-void __intr_keyboard_handler() {
+static keyboard_state_t keyboard_state;
+
+static uint8_t scan_code_mapping[80] =
+{
+    0, 0,
+    '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=',
+    0, 0,
+    'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']',
+    0, 0,
+    'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`',
+    0,
+    '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/',
+    0,
+    '*',
+    0,
+    ' ',
+    0,
+};
+
+// static int rtc_state = 0;
+
+DEFINE_DEVICE_HANDLER(keyboard) {
     cli();
     send_eoi(KEYBOARD_IRQ);
-    unsigned char scan_code = inb(0x60);
-    printf("Keyboard interrupt, scan code: 0x%x\n", scan_code);
+    unsigned char scan_code = inb(KEYBOARD_PORT);
+    // printf("Keyboard interrupt, scan code: 0x%x\n", scan_code);
+    if (scan_code > 80)
+        return;
+    if (scan_code >= 2 && scan_code <= 0x80) {
+        putc(scan_code_mapping[scan_code]);
+    }
+    // if (scan_code == SCANCODE_LSHIFT_PRESSED) {
+    //     keyboard_state.shift = 1;
+    //     if (rtc_state == 0) {
+    //         rtc_state = 1;
+            
+    //     } else {
+    //         rtc_state = 0;
+
+    //     }
+    // }
     sti();
 }
 
-void keyboard_init() {
+DEFINE_DEVICE_INIT(keyboard) {
     printf("Keyboard_init\n");
+    keyboard_state.shift = 0;
+    keyboard_state.caps = 0;
+    keyboard_state.ctrl = 0;
+    keyboard_state.alt = 0;
     enable_irq(KEYBOARD_IRQ);
 }
--- student-distrib/devices/keyboard.h
+++ student-distrib/devices/keyboard.h
@@ -1,9 +1,24 @@
-#ifndef KEYBOARD_H
-#define KEYBOARD_H
+#ifndef _KEYBOARD_H
+#define _KEYBOARD_H
+
+#include "device_utils.h"
 
 #define KEYBOARD_IRQ 1
+#define KEYBOARD_PORT 0x60
 
-extern void __intr_keyboard_handler();
-extern void keyboard_init();
+#define SCANCODE_LSHIFT_PRESSED 0x2A
+#define SCANCODE_LSHIFT_RELEASED 0xAA
+#define SCANCODE_RSHIFT_PRESSED 0x36
+#define SCANCODE_RSHIFT_RELEASED 0xB6
 
-#endif /* KEYBOARD_H */
+typedef struct {
+    int shift;
+    int caps;
+    int ctrl;
+    int alt;
+} keyboard_state_t;
+
+DECLARE_DEVICE_HANDLER(keyboard);
+DECLARE_DEVICE_INIT(keyboard);
+
+#endif /* _KEYBOARD_H */

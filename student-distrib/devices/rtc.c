1e42e9037252d2705805015b93dd7fa5d2275b61
Charles.Wen <jwen14@illinois.edu>
2023-10-14 21:36:08 -0500

    PIC and RTC done v1

---
+++ student-distrib/devices/rtc.c
@@ -0,0 +1,35 @@
+#include "rtc.h"
+#include "../lib.h"
+//#include "../i8259.h"
+
+uint32_t rtc_time_counter;
+
+void RTC_init(void) {
+    char prev;
+    outb(RTC_B, RTC_PORT);     // select register B, and disable NMI
+    prev = inb(RTC_CMOS_PORT); // read the current value of register B
+    outb(RTC_B, RTC_PORT);     // set the index again (inb will reset the idx)
+    /* write the previous value ORed with 0x40. This turns on bit 6 of register B */
+    outb(prev | 0x40, RTC_CMOS_PORT);
+
+    outb(RTC_A, RTC_PORT);     // set index to register A, disable NMI
+    prev = inb(RTC_CMOS_PORT); // get the previous value of register B
+    outb(RTC_A, RTC_PORT);     // set the index again
+    outb((prev & 0xF0) | RTC_BASE_FRE, RTC_CMOS_PORT);  // set the frequency to 1024
+
+    rtc_time_counter = 0;
+    enable_irq(RTC_IRQ);
+
+}
+
+void __intr_RTC_handler(void) {
+    rtc_time_counter ++;
+
+    /* for cp1 */
+    test_interrupts();    
+
+    outb(RTC_C, RTC_CMOS_PORT); // select register C
+    inb(RTC_CMOS_PORT);		    // just throw away contents
+
+    send_eoi(RTC_IRQ);
+}

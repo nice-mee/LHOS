fa95e530da425aadb251e07b5b40b5905ff5f043
Chiming Ni <chiming2@illinois.edu>
2023-10-22 12:09:12 -0500

    [Checkpoint 2] Fix final merge problems

--- student-distrib/tests.c
+++ student-distrib/tests.c
@@ -336,100 +336,6 @@ int fread_test(const uint8_t* fname){
 	return PASS;
 }
 
-int read_dentry_by_name_test(const uint8_t* fname){
-	TEST_HEADER;
-
-	dentry_t dentry;
-	int32_t result;
-
-	result = read_dentry_by_name(fname, &dentry);
-	if(result == -1 || strncmp((int8_t*)dentry.file_name, (int8_t*)fname, MAX_FILE_NAME) != 0) return FAIL;
-	printf("The dentry's filename is ");
-	vt_write(1, dentry.file_name, MAX_FILE_NAME);
-	printf("\n");
-	printf("The dentry's filetype is %u\n", dentry.file_type);
-	printf("The dentry's inode index is %u\n", dentry.inode_index);
-	return PASS;
-}
-
-int read_dentry_by_index_test(uint32_t index){
-	TEST_HEADER;
-
-	dentry_t dentry;
-	int32_t result;
-
-	if(read_dentry_by_index(63, &dentry) != -1) return FAIL;
-	if(read_dentry_by_index(0, &dentry) == -1) return FAIL;
-
-	result = read_dentry_by_index(index, &dentry);
-	if(result == -1) return FAIL;
-	printf("The dentry's filename is ");
-	vt_write(1, dentry.file_name, MAX_FILE_NAME);
-	printf("\n");
-	printf("The dentry's filetype is %u\n", dentry.file_type);
-	printf("The dentry's inode index is %u\n", dentry.inode_index);		// index is not the inode index
-	return PASS;
-}
-
-int dir_read_test(){
-	TEST_HEADER;
-	
-	uint32_t i;
-	uint8_t buf[MAX_FILE_NAME];
-
-	dir_open(NULL);
-	if(dir_read(0, NULL, 0) != -1) return FAIL;
-	dir_close(0);
-
-	dir_open(NULL);
-	for(i = 0; i < MAX_FILE_NUM; i++){
-		if(dir_read(0, buf, 0) == -1) return PASS;
-		printf("The %u dentry has file name ", i);
-		vt_write(1, buf, MAX_FILE_NAME);
-		printf("\n");
-	}
-	return FAIL;
-}
-
-int fread_test(const uint8_t* fname){
-	TEST_HEADER;
-	
-	uint8_t buf1[1024] = {'f', 'i', 'l', 'e', ' ', 'r', 'e', 'a', 'c', 'h', ' ', 't', 'h', 'e', ' ', 'e', 'n', 'd', '\n', 0};
-	uint8_t buf2[4096] = {'f', 'i', 'l', 'e', ' ', 'r', 'e', 'a', 'c', 'h', ' ', 't', 'h', 'e', ' ', 'e', 'n', 'd', '\n', 0};
-	uint8_t buf3[2048] = {'f', 'i', 'l', 'e', ' ', 'r', 'e', 'a', 'c', 'h', ' ', 't', 'h', 'e', ' ', 'e', 'n', 'd', '\n', 0};
-	uint8_t buf4[4096 + 1024] = {'f', 'i', 'l', 'e', ' ', 'r', 'e', 'a', 'c', 'h', ' ', 't', 'h', 'e', ' ', 'e', 'n', 'd', '\n', 0};
-
-	if(fopen(fname) == -1) return FAIL;
-	if(fread(0, buf1, 1024) == -1) return FAIL;
-	printf("First reading result:\n");
-	vt_write(1, buf1, 1024);
-	printf("\n");
-	if(fread(0, buf2, 3090) == -1) return FAIL;
-	printf("Second reading result:\n");
-	vt_write(1, buf2, 3090);
-	printf("\n");
-	if(fread(0, buf3, 2048) == -1) return FAIL;
-	printf("Third reading result:\n");
-	vt_write(1, buf3, 2048);
-	printf("\n");
-	if(fread(0, buf4, 4096) == -1) return FAIL;
-	printf("Forth reading result:\n");
-	vt_write(1, buf4, 4096);
-	printf("\n");
-	fclose(0);
-
-	/*
-	fopen(fname);
-	if(fread(0, buf4, 4096) == -1) return FAIL;
-	printf("First reading result:\n");
-	vt_write(1, buf4, 4096);
-	printf("\n");
-	fclose(0);
-	*/
-
-	return PASS;
-}
-
 
 int RTC_change_freq() {
 	TEST_HEADER;
@@ -465,22 +371,20 @@ void launch_tests(){
 	// TEST_OUTPUT("deref_video_mem_lowerbound", deref_video_mem_lowerbound());
 	// TEST_OUTPUT("deref_ker_mem_upperbound", deref_ker_mem_upperbound());
 	// TEST_OUTPUT("deref_ker_mem_lowerbound", deref_ker_mem_lowerbound())
-	//TEST_OUTPUT("deref_video_mem", deref_video_mem());
-	//TEST_OUTPUT("deref_ker_mem", deref_ker_mem());
-	//TEST_OUTPUT("terminal_read_test", terminal_read_test());
-	//TEST_OUTPUT("terminal_write_test", terminal_write_test());
+	// TEST_OUTPUT("deref_video_mem", deref_video_mem());
+	// TEST_OUTPUT("deref_ker_mem", deref_ker_mem());
+	// TEST_OUTPUT("terminal_read_test", terminal_read_test());
+	// TEST_OUTPUT("terminal_write_test", terminal_write_test());
 
 	// launch your tests here
 	TEST_OUTPUT("read_dentry_by_index_test", read_dentry_by_index_test(0)); // this one is derictory
 	TEST_OUTPUT("read_dentry_by_index_test", read_dentry_by_index_test(1));	// this one is regular file
 	TEST_OUTPUT("read_dentry_by_index_test", read_dentry_by_index_test(5));	// this one is rtc
-	/*
-	TEST_OUTPUT("read_dentry_by_name_test", read_dentry_by_name_test("BYDBYDBYD"));
-	TEST_OUTPUT("read_dentry_by_name_test", read_dentry_by_name_test("."));
-	TEST_OUTPUT("read_dentry_by_index_test", read_dentry_by_index_test(10));
-	TEST_OUTPUT("read_dentry_by_name_test", read_dentry_by_name_test("frame0.txt"));
-	TEST_OUTPUT("read_dentry_by_name_test", read_dentry_by_name_test("verylargetextwithverylongname.txt"));
-	*/
+	// TEST_OUTPUT("read_dentry_by_name_test", read_dentry_by_name_test("BYDBYDBYD"));
+	// TEST_OUTPUT("read_dentry_by_name_test", read_dentry_by_name_test("."));
+	// TEST_OUTPUT("read_dentry_by_index_test", read_dentry_by_index_test(10));
+	// TEST_OUTPUT("read_dentry_by_name_test", read_dentry_by_name_test("frame0.txt"));
+	// TEST_OUTPUT("read_dentry_by_name_test", read_dentry_by_name_test("verylargetextwithverylongname.txt"));
 	// TEST_OUTPUT("dir_read_test", dir_read_test());
 	// TEST_OUTPUT("fread_test", fread_test("frame0.txt"));
 	// TEST_OUTPUT("fread_test", fread_test("verylargetextwithverylongname.txt"));

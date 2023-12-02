3cae599dd2ff9e13940ead6698d987701593f044
Chiming Ni <chiming2@illinois.edu>
2023-12-02 17:53:08 -0600

    [NANI] ioctl syscall

--- syscalls/ece391syscall.h
+++ syscalls/ece391syscall.h
@@ -21,6 +21,7 @@ extern int32_t ece391_getargs (uint8_t* buf, int32_t nbytes);
 extern int32_t ece391_vidmap (uint8_t** screen_start);
 extern int32_t ece391_set_handler (int32_t signum, void* handler);
 extern int32_t ece391_sigreturn (void);
+extern int32_t ece391_ioctl(int32_t fd, int32_t flag);
 
 enum signums {
 	DIV_ZERO = 0,
